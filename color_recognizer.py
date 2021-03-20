4101af49d5663848740b05766dd7d221a4922668
Pranshul Kaushik <pranshul47007.pk@gmail.com>
2021-03-20 11:17:18 +0530

    added a module named rectifier and made colors RBG more dynamic

--- color_recognizer.py
+++ color_recognizer.py
@@ -4,6 +4,7 @@ from colormath.color_objects import sRGBColor, LabColor
 from colormath.color_conversions import convert_color
 from colormath.color_diff import delta_e_cie2000
 from scipy import stats
+import colors
 import random
 import os
 
@@ -16,13 +17,13 @@ def ColorDistance(rgb1,rgb2):
     delta_e = delta_e_cie2000(color1_lab, color2_lab)
     return delta_e
 
-def collect_init_samples(arr, _, color = None):
-    yellow = [161, 117, 3]
-    green = [5, 68, 6]
-    orange = [178, 48, 5]
-    red = [164, 8, 21]
-    white = [162, 162, 164]
-    blue = [5, 35, 126]
+def collect_samples(arr, _):
+    yellow = colors.yellow
+    green = colors.green
+    orange = colors.orange
+    red = colors.red
+    white = colors.white
+    blue = colors.blue
 
     """orange = [255,69,0]
     yellow = [255,255,0]
@@ -31,94 +32,45 @@ def collect_init_samples(arr, _, color = None):
     blue = [0,0,139]
     green = [0,128,0] """
 
-    if color == None:
-        orange_dist = []
-        yellow_dist = []
-        white_dist = []
-        red_dist = []
-        blue_dist = []
-        green_dist = []
-    else:
-        problem_color =  {
-            color[0] : [],
-            color[1] : []
-        }
+    orange_dist = []
+    yellow_dist = []
+    white_dist = []
+    red_dist = []
+    blue_dist = []
+    green_dist = []
+
     while _:
         x = np.random.randint(arr.shape[0])
         y = np.random.randint(arr.shape[1])
         pix = arr[x][y]
-        if color == None:
-            orange_dist += [ColorDistance(pix, orange)] 
-            yellow_dist += [ColorDistance(pix, yellow)]
-            white_dist += [ColorDistance(pix, white)]
-            red_dist += [ColorDistance(pix, red)]
-            blue_dist += [ColorDistance(pix, blue)]
-            green_dist += [ColorDistance(pix, green)]
-        else:
-            if "orange" in problem_color.keys():
-                problem_color["orange"] += [ColorDistance(pix, orange)]
-            if "yellow" in problem_color.keys():
-                problem_color["yellow"] += [ColorDistance(pix, yellow)]
-            if "white" in problem_color.keys():
-                problem_color["white"] += [ColorDistance(pix, white)]
-            if "red" in problem_color.keys():
-                problem_color["red"] += [ColorDistance(pix, red)]
-            if "blue" in problem_color.keys():
-                problem_color["blue"]+= [ColorDistance(pix, blue)]
-            if "green" in problem_color.keys():
-                problem_color["green"]+= [ColorDistance(pix, green)]
+        orange_dist += [ColorDistance(pix, orange)] 
+        yellow_dist += [ColorDistance(pix, yellow)]
+        white_dist += [ColorDistance(pix, white)]
+        red_dist += [ColorDistance(pix, red)]
+        blue_dist += [ColorDistance(pix, blue)]
+        green_dist += [ColorDistance(pix, green)]
         _ -= 1 
-    if color == None:
-        return {
-            "orange": orange_dist,
-            "yellow": yellow_dist,
-            "white" : white_dist, 
-            "red"   : red_dist, 
-            "blue"  : blue_dist,
-            "green" : green_dist
-              }
-    else:
-        return problem_color
+    return {
+        "orange": orange_dist,
+        "yellow": yellow_dist,
+        "white" : white_dist, 
+        "red"   : red_dist, 
+        "blue"  : blue_dist,
+        "green" : green_dist
+            }
 
-def color_detector(arr, to_be_careful = False):
-    dist = collect_init_samples(arr, 100)
-    number_sample = 100
+def color_detector(arr):
+    number_sample = 300
+    dist = collect_samples(arr, number_sample)
     flag = 0
-    if not to_be_careful:
-        while flag != 6:
-            min_c = random.choice(list(dist.keys()))
-            min_c_dist = dist[min_c]
-            flag = 0
-            for each_c in dist.keys():
-                statistic, p_value = stats.ttest_ind(dist[each_c], min_c_dist)
-                if statistic < 0:
-                    min_c = each_c
-                    min_c_dist = dist[min_c]
-                flag += 1
-    else:
-        while flag != 6:
-            min_c = random.choice(list(dist.keys()))
-            min_c_dist = dist[min_c]
-            number_sample = 100
-            p_value = 1
-            for each_c in dist.keys():
-                statistic, p_value = stats.ttest_ind(dist[each_c], min_c_dist)
-                while p_value < 0.1 and statistic < 0:
-                    statistic, p_value = stats.ttest_ind(dist[each_c], min_c_dist)
-                    number_sample *= 4
-                    new_dist_samples = collect_init_samples(arr, number_sample, color = [min_c, each_c])
-                    for each_new_c in new_dist_samples.keys():
-                        dist[each_new_c] += new_dist_samples[each_new_c]
-                    if p_value < 0.1:
-                        break
-                    print(statistic, p_value)
-                if statistic < 0:
-                    min_c = each_c
-                    min_c_dist = dist[min_c]
-                flag += 1
+    while flag != 6:
+        min_c = random.choice(list(dist.keys()))
+        min_c_dist = dist[min_c]
+        flag = 0
+        for each_c in dist.keys():
+            statistic, p_value = stats.ttest_ind(dist[each_c], min_c_dist)
+            if statistic < 0:
+                min_c = each_c
+                min_c_dist = dist[min_c]
+            flag += 1
     return min_c
-
-""" color = "red"
-img = Image.open(os.path.dirname(__file__) + f'/division/{color}.jpeg')
-arr = np.array(img)
-print(color_detector(arr)) """
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from PIL import Image
 import numpy as np
-import partitioner, color_recognizer
+import partitioner, color_recognizer, colors
 import os
+
+def rectifier(pos, label):
+    img = Image.open(os.path.dirname(__file__) + f'/division/{pos}.jpeg')
+    arr = np.array(img)
+    exec(f"colors.{label} = list(map(int, arr.mean(axis = 1).mean(0)))")
 
 img = Image.open(os.path.dirname(__file__) + f'/media/face.jpeg')
 arr = np.array(img)
 div = partitioner.divide_face(arr)
 for div_name, div_arr in div.items():
-    print(div_name, color_recognizer.color_detector(div_arr, to_be_careful= False))
+    print(div_name, color_recognizer.color_detector(div_arr))
+
+ok = int(input("\n\nCorrrect (0 No/1 Yes):- "))
+if ok == 0:
+    _ = int(input("How many are wrong :- "))
+    while _:
+        rectifier(*input("/nPos and Correct color :- ").split())
+        _ -= 1
+    
+    for div_name, div_arr in div.items():
+        print(div_name, color_recognizer.color_detector(div_arr))
